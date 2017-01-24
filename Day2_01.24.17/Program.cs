0c153da87b573353b976253150b204bf4743029b
ksindorf000 <ksindorf000@gmail.com>
2017-01-24 11:42:46 -0500

    Added Day 2 Notes and Database

--- Day2_01.24.17/Program.cs
+++ Day2_01.24.17/Program.cs
@@ -26,11 +26,38 @@ namespace Day2_01._24._17
              * 
              * Use Foreign Keys and Constraints to link tables
              * 
+             * Foreign Keys - 10:53AM
+             * Relates to the Primary Key in another table
+             * 
+             * JOINS - 11:29AM
              * INNER JOIN = only show the ones that fit criteria
              * OUTER JOIN = only show the ones that don't fit criteria
              * 
+             * Schema = structure (What does the database table hold?)
              */
 
+
+            /* --------------HOMEWORK - 10:39AM----------------
+             * 
+             * Create Table
+             * Update
+             *  Generate Script
+             * Save script as .sql in your database project
+             * Add as Existing Item to database project
+             * Update
+             *  Update Table
+             * Refresh Server Explorer
+             * Right Click on Table
+             *  Show Table Data
+             *  
+             * Right Click Tables
+             *  New Query
+             *  
+             * CTRL+SHIFT+E = Execute Query
+             *  
+             * Save to Project
+             * Add as Existing Item to database project
+             * */
         }
     }
 }
---
+++ Day2_Database/Basic_Selects.sql
@@ -0,0 +1,31 @@
+﻿--Find all entries in Band table
+SELECT
+	*
+FROM 
+	Band;
+
+--Find all album titles of genre 'Punk'
+SELECT 
+	Title
+FROM
+	Album
+WHERE
+	Genre = 'Punk';
+
+--Find Titles & Release with word 'Up'
+SELECT
+	Title, ReleaseDate
+FROM
+	Album
+WHERE
+	Title LIKE '%Up%' --% is called 'wild card' 
+
+--Join Tables 
+SELECT
+	b.Name, a.Title
+FROM 
+	Band b
+INNER JOIN
+	Album a ON a.Band = b.Id
+WHERE
+	b.Name = 'NOFX';
