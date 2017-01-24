d7fa572f3a318351494cc5b2d56aa8140b64e34b
ksindorf000 <ksindorf000@gmail.com>
2017-01-24 09:42:23 -0500

    Added Day 2: Intro to Databases

---
+++ Day2_01.24.17/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_01._24._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*-----------INTRO TO DATABASES------------
+             * 
+             * SQL: Standard Query Language
+             * 
+             * Databases = Table + Table
+             * Table = Columns + Rows
+             *  Columns = Attributes pertaining to entities
+             *  Rows = Values pertaining to one entity
+             * 
+             * Rows per table can be as big as the storage 
+             * that's available
+             * 
+             * Columns per table are limited to 64,000
+             * 
+             * Use Foreign Keys and Constraints to link tables
+             * 
+             * INNER JOIN = only show the ones that fit criteria
+             * OUTER JOIN = only show the ones that don't fit criteria
+             * 
+             */
+
+        }
+    }
+}
