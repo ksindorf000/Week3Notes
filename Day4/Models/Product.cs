91ece25fd0c3a9d4f6fb55bc882d6ea2c6fa19dc
ksindorf000 <ksindorf000@gmail.com>
2017-01-26 10:00:22 -0500

    Added Day4

---
+++ Day4/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* -------------- DATABASE SCHEMA DESIGN -------------------
+             * Diagram: https://drive.google.com/file/d/0B8ie37YIHMJUZmIyYjQyU041SHM/view?usp=sharing
+             *  
+             * 
+             * 
+             * 
+             */
+
+        }
+    }
+}
